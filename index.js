3cd23d6ef3f33622581e1c952edb0296a9f8fabe
Xotic750 <xotic750@gmail.com>
2017-07-10 11:39:36 +0200

    Update dependencies

--- index.js
+++ index.js
@@ -1,32 +1,7 @@
 /**
- * @file
- * <a href="https://travis-ci.org/Xotic750/regexp-escape-x"
- * title="Travis status">
- * <img src="https://travis-ci.org/Xotic750/regexp-escape-x.svg?branch=master"
- * alt="Travis status" height="18">
- * </a>
- * <a href="https://david-dm.org/Xotic750/regexp-escape-x"
- * title="Dependency status">
- * <img src="https://david-dm.org/Xotic750/regexp-escape-x.svg"
- * alt="Dependency status" height="18"/>
- * </a>
- * <a href="https://david-dm.org/Xotic750/regexp-escape-x#info=devDependencies"
- * title="devDependency status">
- * <img src="https://david-dm.org/Xotic750/regexp-escape-x/dev-status.svg"
- * alt="devDependency status" height="18"/>
- * </a>
- * <a href="https://badge.fury.io/js/regexp-escape-x" title="npm version">
- * <img src="https://badge.fury.io/js/regexp-escape-x.svg"
- * alt="npm version" height="18">
- * </a>
- *
- * ECMAScript proposed RegExp.escape.
- *
- * Requires ES3 or above.
- *
+ * @file ECMAScript proposed RegExp.escape.
  * @see {@link https://github.com/benjamingr/RegExp.escape|RegExp.escape}
- *
- * @version 1.3.0
+ * @version 1.4.0
  * @author Xotic750 <Xotic750@gmail.com>
  * @copyright  Xotic750
  * @license {@link <https://opensource.org/licenses/MIT> MIT}
