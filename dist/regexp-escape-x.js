e31d213ac8fc5a31db82160558f8c59266a6417b
Graham Fairweather <xotic750@gmail.com>
2019-07-18 01:23:54 +0200

    :bookmark: v3.0.13
    Update dependencies

--- dist/regexp-escape-x.js
+++ dist/regexp-escape-x.js
@@ -2,13 +2,13 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2015-present",
-  "date": "2019-07-17T18:19:07.441Z",
+  "date": "2019-07-17T23:23:49.745Z",
   "describe": "",
   "description": "ECMAScript proposed RegExp.escape.",
   "file": "regexp-escape-x.js",
   "hash": "be3d9a69327eb848a97a",
   "license": "MIT",
-  "version": "3.0.12"
+  "version": "3.0.13"
 }
 */
 (function webpackUniversalModuleDefinition(root, factory) {
