787e64b6a5940db1464f3e4ce738c8a90f61a202
Graham Fairweather <xotic750@gmail.com>
2019-07-31 23:52:56 +0200

    :bookmark: v3.0.27
    Auto build

--- dist/regexp-escape-x.js
+++ dist/regexp-escape-x.js
@@ -2,13 +2,13 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2015-present",
-  "date": "2019-07-31T19:21:49.424Z",
+  "date": "2019-07-31T21:52:51.154Z",
   "describe": "",
   "description": "ECMAScript proposed RegExp.escape.",
   "file": "regexp-escape-x.js",
   "hash": "be3d9a69327eb848a97a",
   "license": "MIT",
-  "version": "3.0.26"
+  "version": "3.0.27"
 }
 */
 (function webpackUniversalModuleDefinition(root, factory) {
