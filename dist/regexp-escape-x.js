c552eca004e0812b7fc7f08c652d9a8a4a84e03b
Graham Fairweather <xotic750@gmail.com>
2019-08-27 21:44:26 +0200

    Maintenance

--- dist/regexp-escape-x.js
+++ dist/regexp-escape-x.js
@@ -2,7 +2,7 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2015-present",
-  "date": "2019-08-20T19:06:03.864Z",
+  "date": "2019-08-27T19:44:19.528Z",
   "describe": "",
   "description": "ECMAScript proposed RegExp.escape.",
   "file": "regexp-escape-x.js",
